c42316760e8309441dfd7fa5567102d880c7039f
TgeorgeT <george_teodorescu2002@yahoo.com>
2024-12-02 23:30:59 +0200

    Add scatterv and gatherv

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     grid_setup(&grid);
     MPI_Barrier(grid.grid_comm);
 
-    const int MATRIX_SIZE = 30;
+    const int MATRIX_SIZE = 1024;
     int block_size = MATRIX_SIZE / grid.dim;
 
     // std::cout << "bsize: " << block_size << "\n";
@@ -25,83 +25,83 @@ int main(int argc, char *argv[])
     int *matrix_b = nullptr;
     int *matrix_c = nullptr;
 
+    // std::cout << "block size " << block_size << "\n";
+
     if (grid.rank == 0)
     {
         matrix_a = generate_matrix(MATRIX_SIZE);
         matrix_b = generate_matrix(MATRIX_SIZE);
         matrix_c = new int[MATRIX_SIZE * MATRIX_SIZE];
 
-        std::cout << "Matrix A:\n";
+        // std::cout << "Matrix A:\n";
         // print_matrix(matrix_a, MATRIX_SIZE);
-        std::cout << "Matrix B:\n";
+        // std::cout << "Matrix B:\n";
         // print_matrix(matrix_b, MATRIX_SIZE);
     }
 
-    std::cout << "got here\n";
+    // std::cout << "got here\n";
 
     int *local_a = new int[block_size * block_size];
     int *local_b = new int[block_size * block_size];
     int *local_c = new int[block_size * block_size];
 
     MPI_Datatype block_type;
-    MPI_Type_vector(block_size, block_size, MATRIX_SIZE, MPI_INT, &block_type);
+    // MPI_Type_vector(block_size, block_size, MATRIX_SIZE, MPI_INT, &block_type);
     MPI_Type_commit(&block_type);
 
-    for (int i = 0; i < grid.dim; ++i)
-    {
-        for (int j = 0; j < grid.dim; ++j)
-        {
-            int rank = i * grid.dim + j; // Rank in the grid
-            if (grid.rank == 0)
-            {
-                int offset = i * MATRIX_SIZE * block_size + j * block_size;
-                MPI_Send(matrix_a + offset, 1, block_type, rank, 0, grid.grid_comm);
-            }
-            if (grid.rank == rank)
-            {
-                MPI_Recv(local_a, block_size * block_size, MPI_INT, 0, 0, grid.grid_comm, MPI_STATUS_IGNORE);
-            }
-        }
-    }
+    int sizes[2] = {MATRIX_SIZE, MATRIX_SIZE};
+    int subsizes[2] = {block_size, block_size};
+    int starts[2] = {0, 0};
 
-    std::cout << "got here1\n";
+    MPI_Datatype subarray_type, resized_type;
+    MPI_Type_create_subarray(2, sizes, subsizes, starts, MPI_ORDER_C, MPI_INT, &subarray_type);
+    MPI_Type_create_resized(subarray_type, 0, block_size * sizeof(int), &resized_type);
+    MPI_Type_commit(&resized_type);
 
-    for (int i = 0; i < grid.dim; ++i)
+    int *sendcounts = nullptr;
+    int *displs = nullptr;
+
+    if (grid.rank == 0)
     {
-        for (int j = 0; j < grid.dim; ++j)
+        displs = new int[grid.dim * grid.dim];
+        sendcounts = new int[grid.dim * grid.dim];
+        std::cout << "grid dim: " << grid.dim << "\n";
+        for (int i = 0; i < grid.dim * grid.dim; i++)
+            sendcounts[i] = 1;
+        int disp = 0;
+        for (int i = 0; i < grid.dim; i++)
         {
-            int rank = i * grid.dim + j; // Rank in the grid
-            if (grid.rank == 0)
+            for (int j = 0; j < grid.dim; j++)
             {
-                int offset = i * MATRIX_SIZE * block_size + j * block_size;
-                MPI_Send(matrix_b + offset, 1, block_type, rank, 1, grid.grid_comm);
+                displs[i * grid.dim + j] = disp;
+                disp += 1;
             }
-            if (grid.rank == rank)
-            {
-                MPI_Recv(local_b, block_size * block_size, MPI_INT, 0, 1, grid.grid_comm, MPI_STATUS_IGNORE);
-            }
+            disp += (block_size - 1) * grid.dim;
         }
+
+        for (int i = 0; i < grid.dim * grid.dim; i++)
+        {
+            std::cout << displs[i] << " ";
+        }
+        std::cout << "\n";
     }
+    MPI_Scatterv(matrix_a, sendcounts, displs, resized_type, local_a, block_size * block_size, MPI_INT, 0, grid.grid_comm);
+
+    MPI_Scatterv(matrix_b, sendcounts, displs, resized_type, local_b, block_size * block_size, MPI_INT, 0, grid.grid_comm);
+
+    // std::cout << "rank " << grid.rank << " received: \n";
+    // for (int i = 0; i < block_size; ++i)
+    // {
+    //     for (int j = 0; j < block_size; ++j)
+    //     {
+    //         std::cout << local_a[i * block_size + j] << " ";
+    //     }
+    //     std::cout << "\n";
+    // }
 
     fox_multiply(MATRIX_SIZE, &grid, local_a, local_b, local_c);
 
-    for (int i = 0; i < grid.dim; ++i)
-    {
-        for (int j = 0; j < grid.dim; ++j)
-        {
-            int rank = i * grid.dim + j;
-            if (grid.rank == rank)
-            {
-                MPI_Send(local_c, block_size * block_size, MPI_INT, 0, 2, grid.grid_comm);
-            }
-            if (grid.rank == 0)
-            {
-                int offset = i * MATRIX_SIZE * block_size + j * block_size;
-                MPI_Recv(matrix_c + offset, 1, block_type, rank, 2, grid.grid_comm, MPI_STATUS_IGNORE);
-            }
-        }
-    }
-
+    MPI_Gatherv(local_c, block_size * block_size, MPI_INT, matrix_c, sendcounts, displs, resized_type, 0, grid.grid_comm);
     if (grid.rank == 0)
     {
         int *serial_result = new int[MATRIX_SIZE * MATRIX_SIZE]();
